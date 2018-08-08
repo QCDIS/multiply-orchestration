0090694e5c8ce04e491eb62b792ad769868ac075
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-08 18:42:05 +0200

    removed test

--- test/orchestration/test_frascati_orchestration.py
+++ test/orchestration/test_frascati_orchestration.py
@@ -1,10 +1,10 @@
-from multiply_orchestration.frascati_orchestration import Orchestrator
-import os
-import shutil
+# from multiply_orchestration.frascati_orchestration import Orchestrator
+# import os
+# import shutil
 
-__author__ = "Tonio Fincke (Brockmann Consult GmbH)"
-
-TEST_PATH = './test/test_data/out'
+# __author__ = "Tonio Fincke (Brockmann Consult GmbH)"
+#
+# TEST_PATH = './test/test_data/out'
 
 # def test_orchestrate():
 #     if os.path.exists(TEST_PATH):
