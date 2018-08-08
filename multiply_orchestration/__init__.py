ec24a2792b5b4063d768df0b2ca5871a9f04a268
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-08 18:31:13 +0200

    edited init file

--- multiply_orchestration/__init__.py
+++ multiply_orchestration/__init__.py
@@ -1,2 +1,3 @@
 from multiply_orchestration.orchestration import Orchestrator
 from multiply_orchestration.frascati_orchestration import Orchestrator
+from multiply_orchestration.sym_linker import create_sym_link, create_sym_links
