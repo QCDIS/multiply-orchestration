2fb407ea48a33d3ce0516769ae9047cea11f7fac
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-08 17:19:34 +0200

    disabled unneeded test

--- test/orchestration/test_frascati_orchestration.py
+++ test/orchestration/test_frascati_orchestration.py
@@ -6,9 +6,9 @@ __author__ = "Tonio Fincke (Brockmann Consult GmbH)"
 
 TEST_PATH = './test/test_data/out'
 
-def test_orchestrate():
-    if os.path.exists(TEST_PATH):
-        shutil.rmtree(TEST_PATH)
-    os.mkdir(TEST_PATH)
-    Orchestrator.orchestrate(TEST_PATH)
-    shutil.rmtree(TEST_PATH)
+# def test_orchestrate():
+#     if os.path.exists(TEST_PATH):
+#         shutil.rmtree(TEST_PATH)
+#     os.mkdir(TEST_PATH)
+#     Orchestrator.orchestrate(TEST_PATH)
+#     shutil.rmtree(TEST_PATH)
